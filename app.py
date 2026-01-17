0424d1eafb10c3cb8ede97963aefcf763bd221fc
Amir Sabzalipour <126730174+AmirSabzalipour@users.noreply.github.com>
2026-01-18 00:57:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ div[data-testid="stBottomBlockContainer"] > div {{
   max-width: {RIGHT_PANEL_MAX_WIDTH_PX}px !important;
   width: 100% !important;
 
-  margin: {OUTER_TOP_GAP_PX + RIGHT_PANEL_TOP_EXTRA_PX}px {OUTER_RIGHT_GAP_PX}px {OUTER_BOTTOM_GAP_PX}px {RIGHT_PANEL_LEFT_PX}px !important;
+  margin: {OUTER_TOP_GAP_PX }px {OUTER_RIGHT_GAP_PX}px {OUTER_BOTTOM_GAP_PX}px {RIGHT_PANEL_LEFT_PX}px !important;
   padding: {MAIN_PADDING_PX}px !important;
 
   background: #ffffff !important;
