f7a28e7d4ca6b3e66a5f3fa26727559bed3016bc
Amir Sabzalipour <126730174+AmirSabzalipour@users.noreply.github.com>
2026-02-06 00:04:45 +0100

    Change sidebar background color to light gray

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ section[data-testid="stSidebar"] {{
   min-width: {SIDEBAR_WIDTH_PX}px !important;
   max-width: {SIDEBAR_WIDTH_PX}px !important;
 
-  background: #C0C0C0 !important;
+  background: #D3D3D3 !important;
   border-right: 1px solid rgba(0,0,0,0.06) !important;
 }}
 
